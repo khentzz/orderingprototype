635181961ed0b72590b2bba7c1e84ef3fad26624
khentzz <khentlanzar@gmail.com>
2026-03-28 13:19:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 // ============================================================
 
 window.SHOPGO_CONFIG = {
-  supabaseUrl:  'https://supabase.com/dashboard/project/boupuyrkpfrqonbzhvsh',
+  supabaseUrl:  'https://boupuyrkpfrqonbzhvsh.supabase.co',
   supabaseKey:  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvdXB1eXJrcGZycW9uYnpodnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MjQ4MjEsImV4cCI6MjA5MDIwMDgyMX0.cOGiqJ5oKMhFc0i-b0dfx09Wv7ymooEyWONenR-47i8',
 };
